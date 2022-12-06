e09dd97e8ecf58dab9864c369df7815d3f82feae
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2022-12-06 09:10:37 +0530

    navbar desgine done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Feed from "./pages/Feed";
+import VideoDetail from "./pages/VideoDetail";
+import ChannelDetail from "./pages/ChannelDetail";
+import SerchFeed from "./pages/SearchFeed";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState("");
+  const searching = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar search={search} setSearch={setSearch} searching={searching} />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search" element={<SerchFeed />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = ({ search, setSearch, searching }) => {
+  return (
+    <nav className="bg-slate-800 p-4 h-12 flex items-center justify-between">
+      <div className="logo">
+        <em className="text-white font-bold text-xl">
+          My<span className="text-red-400">Tube</span>
+        </em>
+      </div>
+      <div className="serchbar">
+        <form onSubmit={searching} className="bg-white px-1 rounded-md">
+          <input
+            className="p-1 md:w-[18rem]  border-r-2"
+            type="text"
+            placeholder="search something..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <input className="text-black px-1" type="submit" value="Search" />
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
