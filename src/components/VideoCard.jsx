1701e9acf7c31763aa53bb64edde2cd180eb4313
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2022-12-06 14:29:24 +0530

    thumnails started working

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const VideoCard = () => {
-  return f;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Link to={videoId ? `/video/${videoId}` : null}>
+      <div className="box">
+        <div className="thumnail">
+          <img
+            src={snippet?.thumbnails?.high?.url}
+            alt="Thumbnail not available"
+          />
+        </div>
+        <div className="details"></div>
+      </div>
+    </Link>
+  );
 };
 export default VideoCard;
